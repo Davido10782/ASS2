dbf241e817eef7bf7583b680ccb9f14a45b8c056
David <102192810+Davido10782@users.noreply.github.com>
2022-08-06 23:38:31 +0100

    Add files via upload

---
+++ Day 2 Assignment/js/day2.js
@@ -0,0 +1,85 @@
+// EXERCISE 1 DAY2
+let challenge = '30 Days of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(1,21))
+console.log(challenge.substring(0,2))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+
+let social = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(social.split(', '))
+console.log(challenge.replace('JavaScript', 'python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt(9))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+
+let because = 'You cannot end a sentence with because because because is a conjunction'
+console.log(because.indexOf('because'))
+console.log(because.lastIndexOf('because'))
+console.log(because.search('because'))
+
+console.log(challenge.trim())
+
+console.log(challenge.startsWith('30'))
+console.log(challenge.endsWith('JavaScript'))
+console.log(challenge.match('a'))
+
+let string = '30 Days of'
+console.log(string.concat('JavaScript'))
+
+
+console.log(challenge.repeat( 2 ))
+
+
+
+
+
+// EXERCISE 2
+// question 1
+console.log('The quote' + 'There is no exercise better for the heart than reaching down and lifting people up.' + 'by John Holmes teaches us to help one another.')
+
+// question 2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+// question 3
+let num = 10
+let typeof10 = num
+console.log(typeof num)
+
+// question 4
+let num1 = 9.8
+let numint = 10
+let numFloat = parseFloat(numint)
+console.log(numFloat)
+
+
+// question 5
+let check = "jargon"
+let check1 = "python"
+
+console.log(check.includes('on'))
+console.log(check1.includes('on'))
+
+// question 6
+let hope ='I hope this course is not full of jargon.'
+console.log(hope.includes('jargon'))
+
+// question 7
+console.log(Math.floor(Math.random()* 101))
+
+// question 8
+console.log(Math.floor(Math.random()* (51))+ 50)
+
+// question 9
+console.log(Math.floor(Math.random()*256))
+
+// question 10
+
+let string1 = 'Javascript'
+console.log(string1[5])
+
